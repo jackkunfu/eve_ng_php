5185228cfc568c37bcf2935e9220271a572b7dc8
Your Name <you@example.com>
2021-09-14 16:42:12 +0800

    gx

--- themes/adminLTE/unl_data/js/angularjs/controllers/mainnew1Ctrl.js
+++ themes/adminLTE/unl_data/js/angularjs/controllers/mainnew1Ctrl.js
@@ -44,6 +44,184 @@ function hdlPages(cur, total) {
     return [1, '...', cur - 2, cur - 1, cur, cur + 1, cur + 2, '...', total];
   }
 }
+
+/* download.js */
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.download = factory();
+  }
+})(this, function () {
+  return function download(data, strFileName, strMimeType) {
+    var self = window, // this script is only for browsers anyway...
+      defaultMime = 'application/octet-stream', // this default mime also triggers iframe downloads
+      mimeType = strMimeType || defaultMime,
+      payload = data,
+      url = !strFileName && !strMimeType && payload,
+      anchor = document.createElement('a'),
+      toString = function (a) {
+        return String(a);
+      },
+      myBlob = self.Blob || self.MozBlob || self.WebKitBlob || toString,
+      fileName = strFileName || 'download',
+      blob,
+      reader;
+    myBlob = myBlob.call ? myBlob.bind(self) : Blob;
+
+    if (String(this) === 'true') {
+      //reverse arguments, allowing download.bind(true, "text/xml", "export.xml") to act as a callback
+      payload = [payload, mimeType];
+      mimeType = payload[0];
+      payload = payload[1];
+    }
+
+    if (url && url.length < 2048) {
+      // if no filename and no mime, assume a url was passed as the only argument
+      fileName = url.split('/').pop().split('?')[0];
+      anchor.href = url; // assign href prop to temp anchor
+      if (anchor.href.indexOf(url) !== -1) {
+        // if the browser determines that it's a potentially valid url path:
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', url, true);
+        ajax.responseType = 'blob';
+        ajax.onload = function (e) {
+          download(e.target.response, fileName, defaultMime);
+        };
+        setTimeout(function () {
+          ajax.send();
+        }, 0); // allows setting custom ajax headers using the return:
+        return ajax;
+      } // end if valid url?
+    } // end if url?
+
+    //go ahead and download dataURLs right away
+    if (/^data:([\w+-]+\/[\w+.-]+)?[,;]/.test(payload)) {
+      if (payload.length > 1024 * 1024 * 1.999 && myBlob !== toString) {
+        payload = dataUrlToBlob(payload);
+        mimeType = payload.type || defaultMime;
+      } else {
+        return navigator.msSaveBlob // IE10 can't do a[download], only Blobs:
+          ? navigator.msSaveBlob(dataUrlToBlob(payload), fileName)
+          : saver(payload); // everyone else can save dataURLs un-processed
+      }
+    } else {
+      //not data url, is it a string with special needs?
+      if (/([\x80-\xff])/.test(payload)) {
+        var i = 0,
+          tempUiArr = new Uint8Array(payload.length),
+          mx = tempUiArr.length;
+        for (i; i < mx; ++i) tempUiArr[i] = payload.charCodeAt(i);
+        payload = new myBlob([tempUiArr], { type: mimeType });
+      }
+    }
+    blob = payload instanceof myBlob ? payload : new myBlob([payload], { type: mimeType });
+
+    function dataUrlToBlob(strUrl) {
+      var parts = strUrl.split(/[:;,]/),
+        type = parts[1],
+        indexDecoder = strUrl.indexOf('charset') > 0 ? 3 : 2,
+        decoder = parts[indexDecoder] == 'base64' ? atob : decodeURIComponent,
+        binData = decoder(parts.pop()),
+        mx = binData.length,
+        i = 0,
+        uiArr = new Uint8Array(mx);
+
+      for (i; i < mx; ++i) uiArr[i] = binData.charCodeAt(i);
+
+      return new myBlob([uiArr], { type: type });
+    }
+
+    function saver(url, winMode) {
+      if ('download' in anchor) {
+        //html5 A[download]
+        anchor.href = url;
+        anchor.setAttribute('download', fileName);
+        anchor.className = 'download-js-link';
+        anchor.innerHTML = 'downloading...';
+        anchor.style.display = 'none';
+        anchor.addEventListener('click', function (e) {
+          e.stopPropagation();
+          this.removeEventListener('click', arguments.callee);
+        });
+        document.body.appendChild(anchor);
+        setTimeout(function () {
+          anchor.click();
+          document.body.removeChild(anchor);
+          if (winMode === true) {
+            setTimeout(function () {
+              self.URL.revokeObjectURL(anchor.href);
+            }, 250);
+          }
+        }, 66);
+        return true;
+      }
+
+      // handle non-a[download] safari as best we can:
+      if (/(Version)\/(\d+)\.(\d+)(?:\.(\d+))?.*Safari\//.test(navigator.userAgent)) {
+        if (/^data:/.test(url)) url = 'data:' + url.replace(/^data:([\w\/\-\+]+)/, defaultMime);
+        if (!window.open(url)) {
+          // popup blocked, offer direct download:
+          if (
+            confirm('Displaying New Document\n\nUse Save As... to download, then click back to return to this page.')
+          ) {
+            location.href = url;
+          }
+        }
+        return true;
+      }
+
+      //do iframe dataURL download (old ch+FF):
+      var f = document.createElement('iframe');
+      document.body.appendChild(f);
+
+      if (!winMode && /^data:/.test(url)) {
+        // force a mime that will download:
+        url = 'data:' + url.replace(/^data:([\w\/\-\+]+)/, defaultMime);
+      }
+      f.src = url;
+      setTimeout(function () {
+        document.body.removeChild(f);
+      }, 333);
+    } //end saver
+
+    if (navigator.msSaveBlob) {
+      // IE10+ : (has Blob, but not a[download] or URL)
+      return navigator.msSaveBlob(blob, fileName);
+    }
+
+    if (self.URL) {
+      // simple fast and modern way using Blob and URL:
+      saver(self.URL.createObjectURL(blob), true);
+    } else {
+      // handle non-Blob()+non-URL browsers:
+      if (typeof blob === 'string' || blob.constructor === toString) {
+        try {
+          return saver('data:' + mimeType + ';base64,' + self.btoa(blob));
+        } catch (y) {
+          return saver('data:' + mimeType + ',' + encodeURIComponent(blob));
+        }
+      }
+
+      // Blob but not URL support:
+      reader = new FileReader();
+      reader.onload = function (e) {
+        saver(this.result);
+      };
+      reader.readAsDataURL(blob);
+    }
+    return true;
+  };
+});
+/* end download() */
+
 function mainnew1Controller($scope, $http, $location, $window, $uibModal, $log, $rootScope, FileUploader, focus) {
   console.log('mainnew11');
   $.unblockUI();
@@ -81,6 +259,7 @@ function mainnew1Controller($scope, $http, $location, $window, $uibModal, $log, 
   ];
   $scope.platformList = [];
   $scope.useDescList = [];
+  $scope.quesList = [];
   $scope.labList = [];
   $scope.courseList = [
     { name: 'CCNA', id: 1 },
@@ -126,7 +305,8 @@ function mainnew1Controller($scope, $http, $location, $window, $uibModal, $log, 
     2: '/api/lab/list', // '/api/lab/list'
     3: '/api/student/list', // '/api/lab/list'
     4: '/api/file/list',
-    5: '/api/article/list'
+    5: '/api/article/list',
+    6: '/api/article/list'
   };
 
   $scope.curPage = 1;
@@ -138,7 +318,8 @@ function mainnew1Controller($scope, $http, $location, $window, $uibModal, $log, 
     2: { username: EVENEWUSERNAME, pageSize: 1000 },
     3: { username: EVENEWUSERNAME },
     4: { username: EVENEWUSERNAME },
-    5: { username: EVENEWUSERNAME, category: 'notify' }
+    5: { username: EVENEWUSERNAME, category: 'notify' },
+    6: { username: EVENEWUSERNAME, category: 'question' }
   };
 
   $scope.pageOpt = {
@@ -147,7 +328,8 @@ function mainnew1Controller($scope, $http, $location, $window, $uibModal, $log, 
     2: { cur: 1, size: 1000, total: 0 }, // 配置1000size相当于全量获取
     3: { cur: 1, size: 20, total: 0 },
     4: { cur: 1, size: 15, total: 0 },
-    5: { cur: 1, size: 20, total: 0 }
+    5: { cur: 1, size: 20, total: 0 },
+    6: { cur: 1, size: 4, total: 0 }
   };
 
   $scope.pageArr0 = [];
@@ -156,6 +338,7 @@ function mainnew1Controller($scope, $http, $location, $window, $uibModal, $log, 
   $scope.pageArr3 = [];
   $scope.pageArr4 = [];
   $scope.pageArr5 = [];
+  $scope.pageArr6 = [];
 
   $scope.listKey = {
     0: 'platFormList',
@@ -163,7 +346,8 @@ function mainnew1Controller($scope, $http, $location, $window, $uibModal, $log, 
     2: 'labList',
     3: 'stuCenter',
     4: 'docList',
-    5: 'tongzhiList'
+    5: 'tongzhiList',
+    6: 'quesList'
   };
 
   $scope.clickPage = function (idx, page) {
@@ -251,6 +435,8 @@ function mainnew1Controller($scope, $http, $location, $window, $uibModal, $log, 
   };
 
   $scope.downExcel = function (url, name) {
+    download(url);
+    return;
     // window.open(url);
     // location.href = url;
     let a = document.createElement('a');
